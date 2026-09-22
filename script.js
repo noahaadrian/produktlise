cbcb24fefddf555e82226b5fda4f34d9c6845934
Noah Adrian <noahkristensenhahn@gmail.com>
2026-09-22 15:19:27 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+const endpoint = "https://kea-alt-del.dk/t7/api/products?limit=20";
+const produktliste = document.querySelector(".produktliste");
+
+fetch(endpoint)
+  .then((res) => res.json())
+  .then(visData);
+
+function visData(json) {
+  console.log(json);
+  json.forEach((element) => {
+    produktliste.innerHTML += `<article class="card">
+    <img src=${`https://kea-alt-del.dk/t7/images/webp/640/${element.id}.webp`} alt="produktbillede" />
+        <h2>${element.productdisplayname}</h2>
+        <h3>${element.brandname}</h3>
+        <p>${element.price}</p>
+        <p>${element.subcategory}</p>
+      </article>`;
+  });
+}
